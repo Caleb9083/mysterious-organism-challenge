d5e5b5d605b63c9b65b9505d803e45944ce44715
Caleb9083 <=calebosam12@gmail.com>
2022-06-24 11:45:56 +0000

    Created function mostRelatedInstances() to return an array, method .compareDna(Obj) altered. Project Completed

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ const pAequorFactory = (specimenNum, dna) => {
       let percetage = Math.round((count / 15) * 100);
       let message = `specimen ${this.specimenNum} and specimen ${anotherPAquor.specimenNum} have ${percetage}% DNA in common`;
       console.log(message);
+      return percetage;
     },
     //method that returns true if pAequour will survive
     willLikelySurvive() {
@@ -56,6 +57,22 @@ const pAequorFactory = (specimenNum, dna) => {
       } else {
         return false;
       }
+    },
+    //method to generate the complement of the DNA strand
+    complementStrand() {
+      theComplementStrand = [];
+      for (let i = 0; i < 15; i++) {
+        if (this.dna[i] === "A") {
+          theComplementStrand.push("T");
+        } else if (this.dna[i] === "T") {
+          theComplementStrand.push("A");
+        } else if (this.dna[i] === "G") {
+          theComplementStrand.push("C");
+        } else {
+          theComplementStrand.push("G");
+        }
+      }
+      return theComplementStrand;
     },
   };
 };
@@ -81,8 +98,23 @@ const generatepAequorArray = () => {
   return pAequorArray;
 };
 const the30instances = generatepAequorArray();
-console.log(the30instances);
+
+//funtion to find the most related instances of pAequor.
+const mostRelatedInstances = () => {
+  let mostRelated = 0;
+  let mostRelatedPair = NaN;
+  for (let i = 0; i < 29; i++) {
+    let valueofComparison = the30instances[i].compareDna(the30instances[i + 1]);
+    if (valueofComparison > mostRelated) {
+      mostRelated = valueofComparison;
+      mostRelatedPair = [the30instances[i], the30instances[i + 1]];
+    }
+  }
+  return mostRelatedPair;
+};
 // Driver code
+
+the30instances.forEach((el) => console.log(el.willLikelySurvive()));
 
 const obj1 = pAequorFactory(101, mockUpStrand());
 console.log(obj1);
@@ -94,3 +126,8 @@ console.log(obj2);
 obj1.compareDna(obj2);
 
 console.log(obj1.willLikelySurvive());
+
+console.log(obj1.dna);
+console.log(obj1.complementStrand());
+
+console.log(mostRelatedInstances());
